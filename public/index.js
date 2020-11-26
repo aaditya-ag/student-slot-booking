294c32948f295bdd2340fbd5bcaa43611777da24
Aaditya Agrawal <aaditya1106@gmail.com>
2020-11-26 20:23:26 +0530

    Added form functionalities to show request is being processed

--- public/index.js
+++ public/index.js
@@ -74,6 +74,7 @@ function validate(){
 //++++++++++Utility functions end++++++++++++
 
 const url = "https://script.google.com/macros/s/AKfycbzJ8Nn2ytbGO8QOkGU1kfU9q50RjDHje4Ysphyesyh-osS76wep/exec";
+clearCourses();
 get_Data();
 
 
@@ -139,6 +140,7 @@ async function get_Data(){
         if(validate() === false){            
             return;
         }
+        $("#request-ongoing").text("Your request is being processed...");
         var formData = new FormData( this );
         fetch("/",{
             method: 'post',
@@ -152,6 +154,7 @@ async function get_Data(){
                 alert("There was an error. Please retry");
                 return;
             }
+            $("#request-ongoing").text("");
             alert(obj.status);
             $('form')[0].reset();
             clearCourses();

b9298c027e5ca18a3694c8e6363821acdba293ce
Aaditya Agrawal <aaditya1106@gmail.com>
2020-11-26 20:09:31 +0530

    Error regarding hosting on heroku

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post("/",function(req,res){
     })
 })
 
-app.listen(3000 || process.env.PORT,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 3000");
 })

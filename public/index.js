7598add266da15edd0014f72962e119eebff6c83
Aaditya Agrawal <aaditya1106@gmail.com>
2020-11-26 13:45:09 +0530

    email notif changes

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
-const url = "https://script.google.com/macros/s/AKfycbzJ8Nn2ytbGO8QOkGU1kfU9q50RjDHje4Ysphyesyh-osS76wep/exec";
+/*++++++++++++++++++++++++++++++++++
+++++++++Utility functions+++++++++++
+++++++++++++++++++++++++++++++++++++*/
 
-get_Data();
-
+//Finds dates which lie between T + 4hours and T + 7days
 function isValidDate(timeSlot){
     var d = new Date();
     var timeNow = d.getTime();
@@ -12,23 +13,54 @@ function isValidDate(timeSlot){
     }
     return false;
 }
-async function get_Data(){
-    const response = await fetch(url);
-    const data = await response.json();  
-    
-    
+
+
+function clearCourses(){
     let courses = $("#courses");
     courses.empty();
     courses.append('<option disabled>Choose Course</option>');
     courses.prop('selectedIndex', 0);
+    clearDates();
+}
+
+function clearDates(){
     let dates = $("#dates");
     dates.empty();
     dates.append('<option disabled>Choose Dates</option>');
     dates.prop('selectedIndex', 0);
+    clearSlots();
+}
+
+function clearSlots(){
     let slots = $("#slots");
     slots.empty();
     slots.append('<option disabled>Choose Time Slot</option>');
     slots.prop('selectedIndex', 0);
+}
+
+function validatePhone(){
+    var text = document.getElementById("pno").value;
+    var regx = /^[6-9]\d{9}$/ ;
+    if(regx.test(text))
+        return true;
+    else
+        return false;
+}
+
+//++++++++++Utility functions end++++++++++++
+
+const url = "https://script.google.com/macros/s/AKfycbzJ8Nn2ytbGO8QOkGU1kfU9q50RjDHje4Ysphyesyh-osS76wep/exec";
+get_Data();
+
+
+async function get_Data(){
+    const response = await fetch(url);
+    const data = await response.json();  
+    
+    let courses = $("#courses");
+    clearCourses();
+    let dates = $("#dates");
+    let slots = $("#slots");
     
     
     $.each(data,function(i,option){
@@ -39,10 +71,7 @@ async function get_Data(){
     $("#courses").on("change",function(){
         var course_name = $("#courses :selected").text();
         
-        dates.empty();
-        dates.append('<option disabled>Choose Dates</option>');
-        dates.prop('selectedIndex', 0);
-
+        clearDates();
 
         $.each(data,function(i,option){
             if(option.course_name === course_name){
@@ -62,9 +91,7 @@ async function get_Data(){
 
     $("#dates").on("change",function(){
 
-        slots.empty();
-        slots.append('<option disabled>Choose Time Slot</option>');
-        slots.prop('selectedIndex', 0);
+        clearSlots();
 
 
         var selectedDate = $("#dates :selected").text();
@@ -81,4 +108,27 @@ async function get_Data(){
         });
 
     });
+
+    
+    $('form').on('submit',function(e){
+        e.preventDefault();
+        if(validatePhone() === false){
+            alert("Please correct Phone number");
+            return;
+        }
+        var formData = new FormData( this );
+        fetch("/",{
+            method: 'post',
+            body: formData
+        }).then(function(response){
+            response = response.text();
+            return response;
+        }).then(function(text){
+            alert(text);
+            $('form')[0].reset();
+            clearCourses();
+        });    
+    });
+    
+
 }
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
-express = require("express");
-bodyParser = require("body-parser");
-app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var nodemailer = require('nodemailer');
+const { getMaxListeners } = require("process");
+var upload = multer();
+var app = express();
 
+app.use(express.static(__dirname+"/public"));
 app.get("/",function(req,res){
-    res.send("Hello");
+    res.sendFile(__dirname+"/index.html");
+})
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'taskone.notchup@gmail.com',
+        pass:'task1.notchup'
+    }
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(upload.array());
+app.post("/",function(req,res){
+    var mailOptions = {
+        from: 'taskone.notchup@gmail.com',
+        to: req.body['parent-email'],
+        subject: 'NotchUp Trial Class Booked successfully',
+        text: 'Dear ' + req.body['parent-name']+",\n" + req.body['child-name'] +"'s class at " + req.body.dates + " " + req.body.slots + " has been successfully booked."
+    }
+
+    transporter.sendMail(mailOptions,function(error,info){
+        if(error){
+            res.send("Some error was found, please refill the form");
+        } else{
+            res.send("NotchUp Trial Class Booked successfully");
+        }
+    })
 })
 
 app.listen(3000,function(){
